896383d05f32f286b97e30411cb7f3cfb6c8bb80
unboundfinance <70339346+unboundfinance@users.noreply.github.com>
2025-02-06 12:53:14 +0530

    Update timeline.ts

--- src/app/data/timeline.ts
+++ src/app/data/timeline.ts
@@ -9,12 +9,12 @@ export const roadmapEvents: { [year: string]: RoadmapEvent[] } = {
     {
       date: "March 2020",
       description: "Research and ideation for Unbound begins.",
-      link: "https://x.com/unboundfinance/status/1326652377915678721"
+      
     },
     {
       date: "April 2020",
       description: "First draft of the whitepaper is released.",
-      link: "https://x.com/unboundfinance/status/1326652377915678721"
+      
     },
     {
       date: "May 2020",
@@ -32,7 +32,7 @@ export const roadmapEvents: { [year: string]: RoadmapEvent[] } = {
     {
       date: "December 2020",
       description: "Release of the inaugural official whitepaper.",
-      link: "https://x.com/unboundfinance/status/1326652377915678721"
+      
     },
   ],
   "2021": [
@@ -44,7 +44,7 @@ export const roadmapEvents: { [year: string]: RoadmapEvent[] } = {
     {
       date: "April 14th, 2021",
       description: "Unbound launches Zeta Testnet.",
-      link: "https://x.com/unboundfinance/status/1602156394140078083"
+     
     },
     {
       date: "June 10th, 2021",
@@ -65,21 +65,35 @@ export const roadmapEvents: { [year: string]: RoadmapEvent[] } = {
       link: "https://harmony.one/unbound"
     },
     {
+      date: "September 28th, 2021",
+      description: "Unbound Sandbox Mainnet Is Now Live On Ethereum",
+      link: "https://x.com/unboundfinance/status/1442738278688456705"
+    },
+    {
       date: "November 4th, 2021",
       description: "Unbound goes live on Ethereum mainnet",
       link: "https://x.com/unboundfinance/status/1442738278688456705"
     },
     {
+      date: "November 5th, 2021",
+      description: "Unbound Partnership with Kyber Network",
+      link: "https://x.com/unboundfinance/status/1456615841252790273"
+    },
+    {
       date: "December 13th, 2021",
       description: "Unbound IDO on Polkastarter commences",
       link: "https://polkastarter.com/projects/unbound"
+    },
+    {
+      date: "December 17th, 2021",
+      description: "Unbound UNB deposits and withdrawals now available on MEXC",
+      link: "https://x.com/unboundfinance/status/1471802223441027072"
     }
   ],
   "2022": [
     {
       date: "January 24th, 2022",
       description: "Unbound on-chain staking live on Ethereum and BSC",
-      link: "https://x.com/unboundfinance/status/1536276972900618240"
     },
     {
       date: "March 2nd, 2022",
@@ -87,21 +101,47 @@ export const roadmapEvents: { [year: string]: RoadmapEvent[] } = {
       link: "https://x.com/unboundfinance/status/1498174888955486208"
     },
     {
+      date: "March 3rd, 2022",
+      description: "Unbound Partnership with Quickswap",
+      link: "https://x.com/unboundfinance/status/1499436102931070983?lang=ar"
+    },
+    {
       date: "May 15th, 2022",
       description: "Unbound goes live on BSC mainnet",
       link: "https://bscscan.com/address/unboundfinance"
     },
     {
+      date: "June 13th, 2022",
+      description: "UNB Flexible Staking on Kucoin",
+      link: "https://x.com/unboundfinance/status/1536276972900618240"
+    },
+    {
       date: "August 1st, 2022",
       description: "Unbound partners with Router protocol",
       link: "https://x.com/routerprotocol/status/1581991668496334854"
+    },
+    {
+      date: "September 7th, 2022",
+      description: "Unbound partners with Multichain",
+      link: "https://x.com/unboundfinance/status/1567385109871067136"
+    },
+    {
+      date: "September 14th, 2022",
+      description: "Unbound Live on Fantom mainnet",
+      link: "https://x.com/unboundfinance/status/1569959340232118275"
     }
+
   ],
   "2023": [
     {
       date: "January 8th, 2023",
       description: "Unbound V2 Second Security Audit completed",
       link: "https://unbound.finance/security-audit"
+    },
+    {
+      date: "March 25th, 2023",
+      description: "Unbound live and ready for trading on Changelly",
+      link: "https://x.com/unboundfinance/status/1639506178773508098"
     },
     {
       date: "April 11th, 2023",
